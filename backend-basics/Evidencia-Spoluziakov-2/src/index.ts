26b8f9f6f834e4bba9dfb4cec3b1bbac3cec7e6b
Maxim Podolak <maxim-podolak@students.skyro.ai>
2025-12-09 09:41:32 +0100

    Fixed EVS2
    Fixes:
    - "meaningful" variable names
    - newId is a number 
    - added newClassmate object
    - removed `if else` statements

--- backend-basics/Evidencia-Spoluziakov-2/src/index.ts
+++ backend-basics/Evidencia-Spoluziakov-2/src/index.ts
@@ -7,28 +7,25 @@ const app = new Elysia()
 
   // List Classmates
   .get('/classmates', () => {
-    const data = fs.readFileSync('classmates.json', 'utf-8');
-    return JSON.parse(data);
+    const classmateList = fs.readFileSync('classmates.json', 'utf-8');
+    return JSON.parse(classmateList);
   })
 
 
   // Add Classmates
   .post('/classmates', ({body}: {body: any}) => {
-    const data = JSON.parse(fs.readFileSync('classmates.json', "utf-8")) as any[];
-    let newId
-    if (data.length === 0) {
-      newId = {
-        id: 1
-      }
-    } else {
-      let highestId = Math.max(...data.map(el => Number(el.id) || 0));
-      newId = {
-        id: highestId + 1
-      }
+    const classmateList = JSON.parse(fs.readFileSync('classmates.json', "utf-8")) as any[];
+    let highestId = Math.max(...classmateList.map(el => Number(el.id) || 0));
+    const newClassmateId = highestId + 1
+
+    const newClassmate = {
+      id: newClassmateId,
+      name: body.name,
+      surname: body.surname,
+      age: body.age
     }
-    body.id = newId.id
-    data.push(body)
-    fs.writeFileSync('classmates.json', JSON.stringify(data, null, 2), 'utf-8');
+    classmateList.push(newClassmate)
+    fs.writeFileSync('classmates.json', JSON.stringify(classmateList, null, 2), 'utf-8');
     return {
       status: 'success',
       message: 'Successfully created',
@@ -38,16 +35,16 @@ const app = new Elysia()
 
   // Delete Classmates
   .delete('/classmates/:id', ({params}: {params: any}) => {
-    const data = JSON.parse(fs.readFileSync('classmates.json', "utf-8")) as any[]
-    const id = Number(params.id)
-    const exists = data.find(el => el.id === id)
-    if (!exists) {
+    const classmateList = JSON.parse(fs.readFileSync('classmates.json', "utf-8")) as any[]
+    const classmateId = Number(params.id)
+    const classmateExists = classmateList.find(el => el.id === classmateId)
+    if (!classmateExists) {
       return {
         status: 'error',
         message: 'Cant find user with that id'
       };
     }
-    const updatedList = data.filter(el => el.id !== id)
+    const updatedList = classmateList.filter(el => el.id !== classmateId)
     fs.writeFileSync('classmates.json', JSON.stringify(updatedList, null, 2))
 
     return {
